ddf5cbb01ad527291bf6b960c62658a26e6eef11
lizikang <lizikang@sensetime.com>
2021-03-06 09:55:15 +0000

    add extract embeddings

--- evaluator/evaluators.py
+++ evaluator/evaluators.py
@@ -1,3 +1,4 @@
+import tqdm
 import numpy as np
 import time
 from utils.eval import accuracy
@@ -37,8 +38,27 @@ class Evaluator(object):
         network.train()
         return top1.avg, top5.avg, losses.avg if losses is not None else None
 
-    # @staticmethod
-    # def extract_embedding(network,
-    #             device,
-    #             dataloader):
-    #     pass
+    @staticmethod
+    def extract_embedding(network,
+                        device,
+                        dataloader):
+        tgt_layer = 'avgpool'
+        assert tgt_layer in network._modules, "The target layer: {} is not in the network. The network modules: [{}]".format(tgt_layer, network._modules)
+        embeddings = []
+        network.eval()
+        with torch.no_grad():
+            pbar = tqdm.tqdm(total=len(dataloader.dataset), unit='extracted data batch')
+            for idx, data_iter in enumerate(dataloader):
+                x = data_iter['inputs'].to(device)
+                for name, module in network._modules.items():
+                    x = module(x)
+                    if name == tgt_layer:
+                        x = torch.squeeze(x)
+                        if x.dim == 1:
+                            x = torch.unsqueeze(x, dim=0)
+                        x = x.float().cpu()
+                        embeddings.append(x)
+                        break
+                pbar.update(x.shape[0])
+                pbar.set_description("batch index: {}".format(idx))
+        return embeddings
